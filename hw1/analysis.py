dd74764a44b9c681deb77bf616e90d0aa1e4daf5
jeffrey levesque <jeff1evesque@yahoo.com>
2018-08-13 19:40:34 -0400

    #9: analysis.py, remove unnecessary columns

--- hw1/analysis.py
+++ hw1/analysis.py
@@ -4,3 +4,22 @@ from pandas import read_csv
 
 ## load dataset
 df = read_csv('Donors_Data.csv')
+
+##
+## remove columns
+##
+## @axis, 0 for rows and 1 for columns
+## @inplace, edit dataframe in place
+##
+df.drop(
+    [
+        'Row Id',
+        'Row Id.',
+        'zipconvert_2',
+        'zipconvert_3',
+        'zipconvert_4',
+        'zipconvert_5'
+    ],
+    axis=1,
+    inplace=True
+)
